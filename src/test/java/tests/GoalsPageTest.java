f1ac206d46a4c8ca0ca343284a13ba48c4a68bb7
Daniil Matafonov <danielmt.dm@gmail.com>
2021-03-13 21:39:15 +0300

    add timeouts

--- src/test/java/tests/GoalsPageTest.java
+++ src/test/java/tests/GoalsPageTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.GoalsPage;
 
+import static com.codeborne.selenide.Selenide.sleep;
+
 public class GoalsPageTest extends BaseTest{
 
     private final GoalsPage goalsPage = new GoalsPage();
@@ -16,11 +18,18 @@ public class GoalsPageTest extends BaseTest{
     @Test
     public void createGoal() {
         goalsPage.openGoalsSection();
+        sleep(2000);
         goalsPage.fillGoalName("Купить ноутбук");
+        sleep(2000);
         goalsPage.fillAmount("1000");
+        sleep(2000);
         goalsPage.openGoal();
+        sleep(2000);
         goalsPage.fillSmsGoalCode("1234");
+        sleep(2000);
         goalsPage.approveGoal();
+        sleep(2000);
         goalsPage.validateGoal();
+        sleep(2000);
     }
 }
